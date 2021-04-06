243f072d1335e16a29df1cda181cdaf58a9f4870
Tharindu Rajarathna <tharindurajarathna@gmail.com>
2021-04-06 13:22:57 +0800

    Adding empty workout service

---
+++ workout/src/main/java/io/tharindu/workouttracker/workout/web/rest/WorkoutResource.java
@@ -0,0 +1,15 @@
+package io.tharindu.workouttracker.workout.web.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WorkoutResource {
+
+    @GetMapping("hello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
