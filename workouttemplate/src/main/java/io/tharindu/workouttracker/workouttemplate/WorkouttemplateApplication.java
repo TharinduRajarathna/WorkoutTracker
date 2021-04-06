4b68e9032e5d8728db1921ff187f2f4d0fed9d8e
Tharindu Rajarathna <tharindurajarathna@gmail.com>
2021-04-07 01:30:24 +0800

    gitignore /.mvn/

--- workouttemplate/src/main/java/io/tharindu/workouttracker/workouttemplate/WorkouttemplateApplication.java
+++ workouttemplate/src/main/java/io/tharindu/workouttracker/workouttemplate/WorkouttemplateApplication.java
@@ -2,14 +2,20 @@ package io.tharindu.workouttracker.workouttemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class WorkouttemplateApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WorkouttemplateApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
